8cb50caa3ddc14a17543560b4fccf3a4ed91b5f2
hhursev <hhursev@gmail.com>
2015-02-20 19:43:02 +0200

    Update TODO and example how KNN is used

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ This is based on kaggle chalenge [Titanic: Learning from disasters](https://www.
 - Decision Tree (victoria92)
 - Bayesian classifier (victoria92)
 - Neural network (hhursev)
+
+- insert test.csv data so we can play around
+- probably float down age and fare in the parser
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import csv
+from parser import TrainDataParser
+from algorithms.k_nearest import KNearestNeighbours
 
-data = csv.reader(open("train.csv"))
-header = next(data)
-dataset = []
 
-for row in data:
-    dataset.append(row)
+if __name__ == '__main__':
+    to_determine = {
+        'parch': '0',
+        'cabin': '',
+        'ticket': 'A/5 21171',
+        'pclass': '3',
+        'sibsp': '1',
+        'sex': 'male',
+        'passengerid': '1',
+        'name': 'Braund, Mr. Owen Harris',
+        'age': '22',
+        'embarked': 'S',
+        'fare': '7.25',
+    }
 
-print(dataset)
+    data_api = TrainDataParser()
+    k_nearest_test = KNearestNeighbours(data=data_api.map_data_to_column_names())
+    # print(k_nearest_test.will_survive(this_survived))
