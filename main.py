8378fc6bf68c05e1322c9f0cb25734942ef145da
hhursev <hhursev@gmail.com>
2015-02-22 06:46:29 +0200

    Cycle through all of the test data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parser import TrainDataParser
-from algorithms.decision_tree import id3, process_dataset
+from algorithms.decision_tree import id3, process_dataset, process_entity
+
 
 if __name__ == '__main__':
     exclude_list = ['cabin', 'ticket', 'name']
@@ -11,14 +12,25 @@ if __name__ == '__main__':
     # print(k_nearest_test.will_survive(this_survived))
     dataset = process_dataset(train_data_api.map_data_to_columns_exclude(exclude_list=exclude_list))
 
-    baba = test_data_api.get_with_id('895')
-    baba_result = gendermodel_data_api.get_with_id('895')
-
     guessed_right = 0
 
     for i in range(892, 1310):
         item = test_data_api.get_with_id(str(i), exclude_list=exclude_list)
         result_expected = gendermodel_data_api.get_with_id(str(item['passengerid']))['survived']
-        # result expected is '0' or '1'
 
-    # result = id3( dataset, item)
+        dt = id3(dataset, {
+            'parch': [0, 1, 2],
+            'pclass': ['1', '2', '3'],
+            'sibsp': [0, 1, 2],
+            'sex': ['male', 'female'],
+            'age': [0, 1, 2, None],
+            'embarked': ['S', 'Q', 'C'],
+            'fare': [0, 1, 2],
+        })
+
+        baba = dt.predict_value(process_entity(item))
+
+        if int(result_expected) == baba:
+            guessed_right += 1
+
+    import ipdb; ipdb.set_trace()
